ff2fdf4a2726f680c81aa0e819b98f10b07f98d6
Adrian Ranea <adrianranea@gmail.com>
2017-08-01 22:17:16 +0200

    added documentation page

--- qipy/__init__.py
+++
@@ -1,5 +0,0 @@
-from qipy.quadratic_integer import QuadraticIntegerRing
-from qipy.ideal import Ideal
-from qipy.class_group import IdealClass, ClassGroup
-
-# __all__ = ['quadratic_integer', 'ideal', 'class_group']
